6b43a55c83c6766c4d58b9bd35b1a6b9a0cb680a
vaibhavmishra1 <vaibhavm209625@gmail.com>
2025-09-15 14:46:06 +0530

    bug fix in model

--- coding/language_modelling/nanogpt/myimplementation/model.py
+++ coding/language_modelling/nanogpt/myimplementation/model.py
@@ -1,25 +1,23 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import math
 class BigramGPT(nn.Module):
     def __init__(self, vocab_size, n_embd):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, n_embd)
-    
-    def __call__(self, idx):
-        self.out = self.embedding(idx)
-        return self.out
-    
+
+    def forward(self, idx):
+        return self.embedding(idx)
+
     def generate(self, idx, max_new_tokens):
         # idx will be a B,T tensor
-        
-        # x will be a B,T,C tensor
         for _ in range(max_new_tokens):
-            x = self(idx)
-            x = x [ :, -1, :]
-            probs = F.softmax(x, dim = -1)
+            x = self.forward(idx)
+            x = x[:, -1, :]
+            probs = F.softmax(x, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
-            idx = torch.cat((idx, idx_next), dim = 1)
+            idx = torch.cat((idx, idx_next), dim=1)
         return idx
             
 
@@ -28,24 +26,27 @@ class Attention(nn.Module):
         super().__init__()
         self.n_embd = n_embd
         self.block_size = block_size
-        self.mask = torch.tril(torch.ones(block_size,block_size)) # T,T 
-        self.mask[self.mask == 0] = float('-inf')
-        self.mask = F.softmax(self.mask, dim = -1)
-        
+        # Registered lower-triangular mask buffer (no gradient, moves with module device)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size, dtype=torch.bool)))
+
         self.wk = nn.Linear(n_embd, head_size)
         self.wq = nn.Linear(n_embd, head_size)
         self.wv = nn.Linear(n_embd, head_size)
-        self.block_size = block_size
-    def __call__(self, x):
+
+    def forward(self, x):
+        # x: [B, T, C]
         k = self.wk(x)
         q = self.wq(x)
         v = self.wv(x)
-        kq = q @ k.transpose(1,2)
-        kq = kq / torch.sqrt(torch.tensor(self.block_size))
-        kq = kq.masked_fill(self.mask == 0, float('-inf'))
-        kq = F.softmax(kq, dim = -1)
-        self.out = kq @ v
-        return self.out
+        att = q @ k.transpose(1, 2)  # [B, T, T]
+        head_dim = q.size(-1)
+        att = att * (1.0 / math.sqrt(head_dim))  # scale by sqrt(head_dim)
+        T = x.size(1)
+        mask = self.tril[:T, :T]
+        att = att.masked_fill(~mask, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        out = att @ v  # [B, T, head_size]
+        return out
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, n_embd, block_size, head_size, n_heads, dropout = 0.2):
@@ -57,9 +58,9 @@ class MultiHeadAttention(nn.Module):
         self.attentions = nn.ModuleList([Attention(n_embd, block_size, head_size) for _ in range(n_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
         self.dropout = nn.Dropout(dropout)
-    
-    def __call__(self, x):
-        out = torch.cat([attention(x) for attention in self.attentions], dim = -1)
+
+    def forward(self, x):
+        out = torch.cat([attention(x) for attention in self.attentions], dim=-1)
         out = self.proj(out)
         out = self.dropout(out)
         return out
@@ -89,11 +90,10 @@ class Block(nn.Module):
         self.feed_forward = FeedForward(n_embd, n_embd)
     
     def forward(self, x):
-        x = self.ln1(x)
-        x = self.attention(x) + x
-        x = self.ln2(x)
-        x = self.feed_forward(x) + x
-        return x
+        # Pre-LN residual block
+        y = x + self.attention(self.ln1(x))
+        z = y + self.feed_forward(self.ln2(y))
+        return z
 
 class Transformer(nn.Module):
     def __init__(self, vocab_size, n_embd, block_size, n_heads, n_layers):
@@ -106,13 +106,15 @@ class Transformer(nn.Module):
         self.lm_head = nn.Linear(n_embd, vocab_size)
         self.block_size = block_size
 
-    def __call__(self, idx):
-        x = self.embedding(idx) # B,T C
-        x += self.position_embedding(torch.arange(self.block_size)) # B,T,C
+    def forward(self, idx):
+        # idx: [B, T]
+        B, T = idx.size()
+        x = self.embedding(idx)  # [B, T, C]
+        pos = torch.arange(T, device=idx.device)  # [T]
+        x = x + self.position_embedding(pos)[None, :, :]  # [B, T, C]
         for block in self.blocks:
             x = block(x)
         x = self.ln(x)
-        self.out = self.lm_head(x)
-        
-        return self.out
+        out = self.lm_head(x)
+        return out
     
